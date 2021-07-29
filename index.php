3a48c0bba7b077bb7c54930eb96a2aea4944bd49
susant0 <blufirework@gmail.com>
2021-07-29 19:45:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim ad quasi error maiores corporis itaque exercitationem obcaecati officia iusto, dolor reprehenderit! Beatae dolores excepturi odio aliquid culpa commodi quasi iure!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
